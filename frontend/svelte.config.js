25cfda9e7ad7405df0d42aef21c29e551563fce5
tho-masu <tho-masu@example.com>
2025-06-24 05:57:55 +0900

    c

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,9 @@ const config = {
       precompress: false,
       envPrefix: '',
     })
-  }
+  },
+  // vitePreprocess()を明示的に設定
+  preprocess: vitePreprocess()
 };
 
 export default config;
